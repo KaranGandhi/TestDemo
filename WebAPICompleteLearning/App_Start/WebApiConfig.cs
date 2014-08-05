1ae6165215fb28203ed77e79ec563c5ed45dbd61
KaranGandhi <karangandhi3991@gmail.com>
2014-08-05 15:00:26 +0530

    First Check In of Learning Web Rest API

---
+++ WebAPICompleteLearning/App_Start/WebApiConfig.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using WebAPICompleteLearning.Models;
+
+namespace WebAPICompleteLearning
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //default
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            //modified name
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{orgid}/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional },
+            //    constraints: new { orgid = @"\d+" } 
+            //);
+            //for rpc call
+            //config.Routes.MapHttpRoute(
+            //    name: "RpcApi",
+            //    routeTemplate: "api/{controller}/{action}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+            //for json(override to work for all browser, if content type is not properly defined)
+
+            //formatting using class
+
+            //var fwtMediaFormatter = new FixedWidthTextMediaFormatter();
+            //fwtMediaFormatter.MediaTypeMappings.Add(
+            //new QueryStringMapping("frmt", "fwt",
+            //new MediaTypeHeaderValue("text/plain")));
+            //config.Formatters.Add(fwtMediaFormatter);
+
+
+
+            //config.Formatters.Add(new JsonpMediaTypeFormatter());
+
+            //Beatify JSON
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            //Respoonce type is JSON is all case
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+
+            //To format numbers for different format
+            config.Formatters.JsonFormatter.SerializerSettings.Converters.Add(new NumberConverter());
+
+            //Culture Handler
+            config.MessageHandlers.Add(new CultureHandler());
+
+            //Encoding Handler
+            //config.MessageHandlers.Add(new EncodingHandler());
+            // Other handlers go here
+
+
+            //foreach (var encoding in config.Formatters.JsonFormatter.SupportedEncodings)
+            //{
+            //    System.Diagnostics.Trace.WriteLine(encoding.WebName);
+            //}
+
+            //we retrive array of media type accepted by browser
+            //here we can chnage or do anything what we want
+
+            //content type based on query string
+            //config.Formatters.JsonFormatter.MediaTypeMappings.Add(
+            //    new QueryStringMapping("frmt", "json",
+            //        new MediaTypeHeaderValue("application/json")));
+            //config.Formatters.XmlFormatter.MediaTypeMappings.Add(
+            //    new QueryStringMapping("frmt", "xml",
+            //        new MediaTypeHeaderValue("application/xml")));
+
+            //foreach (var formatter in config.Formatters)
+            //{
+            //    Trace.WriteLine(formatter.GetType().Name);
+            //    Trace.WriteLine("\tCanReadType: " + formatter.CanReadType(typeof(Employee)));
+            //    Trace.WriteLine("\tCanWriteType: " + formatter.CanWriteType(typeof(Employee)));
+            //    Trace.WriteLine("\tBase: " + formatter.GetType().BaseType.Name);
+            //    Trace.WriteLine("\tMedia Types: " + String.Join(", ", formatter.
+            //    SupportedMediaTypes));
+            //}
+
+            // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
+            // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
+            // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
+            //config.EnableQuerySupport();
+
+            // To disable tracing in your application, please comment out or remove the following line of code
+            // For more information, refer to: http://www.asp.net/web-api
+            config.EnableSystemDiagnosticsTracing();
+        }
+    }
+}
---
+++ WebAPICompleteLearning/Models/EncodingSchema.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace WebAPICompleteLearning.Models
+{
+    public class EncodingSchema
+    {
+        private const string IDENTITY = "identity";
+        private IDictionary<string, Func<Stream, Stream>> supported =
+        new Dictionary<string, Func<Stream, Stream>>
+        (StringComparer.OrdinalIgnoreCase);
+        public EncodingSchema()
+        {
+            supported.Add("gzip", GetGZipStream);
+            supported.Add("deflate", GetDeflateStream);
+        }
+
+        // rest of the class members go here
+
+        public Stream GetGZipStream(Stream stream)
+        {
+            return new GZipStream(stream, CompressionMode.Compress, true);
+        }
+        public Stream GetDeflateStream(Stream stream)
+        {
+            return new DeflateStream(stream, CompressionMode.Compress, true);
+        }
+
+        private Func<Stream, Stream> GetStreamForSchema(string schema)
+        {
+            if (supported.ContainsKey(schema))
+            {
+                ContentEncoding = schema.ToLowerInvariant();
+                return supported[schema];
+            }
+            throw new InvalidOperationException(String.Format("Unsupported encoding schema {0}",
+            schema));
+        }
+
+        public string ContentEncoding { get; private set; }
+
+        public Func<Stream, Stream> GetEncoder(HttpHeaderValueCollection<StringWithQualityHeaderValue> list)
+        {
+            // The following steps will walk you through
+            // completing the implementation of this method
+            if (list != null && list.Count > 0)
+            {
+                // More code goes here
+                var headerValue = list.OrderByDescending(e => e.Quality ?? 1.0D).Where(e => !e.Quality.HasValue || e.Quality.Value > 0.0D).FirstOrDefault(e => supported.Keys.Contains(e.Value, StringComparer.OrdinalIgnoreCase));
+                // Case 1: We can support what client has asked for
+                if (headerValue != null)
+                    return GetStreamForSchema(headerValue.Value);
+                // Case 2: Client will accept anything we support except
+                // the ones explicitly specified as not preferred by setting q=0
+                if (list.Any(e => e.Value == "*" &&
+                (!e.Quality.HasValue || e.Quality.Value > 0.0D)))
+                {
+                    var encoding = supported.Keys.Where(se =>
+                    !list.Any(e =>
+                    e.Value.Equals(se, StringComparison.OrdinalIgnoreCase) &&
+                    e.Quality.HasValue &&
+                    e.Quality.Value == 0.0D))
+                    .FirstOrDefault();
+                    if (encoding != null)
+                        return GetStreamForSchema(encoding);
+                }
+
+                // Case 3: Client specifically refusing identity
+                if (list.Any(e => e.Value.Equals(IDENTITY, StringComparison.OrdinalIgnoreCase) &&
+                e.Quality.HasValue && e.Quality.Value == 0.0D))
+                {
+                    throw new NegotiationFailedException();
+                }
+                // Case 4: Client is not willing to accept any of the encodings
+                // we support and is not willing to accept identity
+                if (list.Any(e => e.Value == "*" &&
+                (e.Quality.HasValue || e.Quality.Value == 0.0D)))
+                {
+                    if (!list.Any(e => e.Value.Equals(IDENTITY, StringComparison.OrdinalIgnoreCase)))
+                        throw new NegotiationFailedException();
+                }
+            }
+            // Settle for the default, which is no transformation whatsoever
+            return null;
+        }
+    }
+}
